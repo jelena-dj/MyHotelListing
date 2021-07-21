c64408a511e45c8b92d640ab92c097a94d495c0a
jelena-dj <87725947+jelena-dj@users.noreply.github.com>
2021-07-21 22:41:43 +0200

    Initial commit

---
+++ Data/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyHotelListing.Data
+{
+	public class Country
+	{
+		public int Id { get; set; }  //primary key
+
+		public string Name { get; set; }
+
+		public string ShortName { get; set; }
+	}
+}
